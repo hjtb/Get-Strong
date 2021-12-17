e0e66605a35e2e104112f2e3810b27764e4be846
hjtb <willholtonjtb@gmail.com>
2021-12-17 17:35:18 +0000

    connected mongo

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
-from flask import Flask
+from flask import (Flask, flash, render_template, redirect, request, session, url_for)
+from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
-
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def starting():
-    return "piping up"
+@app.route("/get_strong")
+def get_strong():
+    workouts = mongo.db.workouts.find()
+    return render_template("get_strong.html", workouts=workouts)
 
 
 if __name__ == "__main__":
