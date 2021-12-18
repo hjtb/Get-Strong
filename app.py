e524e113c7b1b33d93752efd9d7c415d3ae042cb
hjtb <willholtonjtb@gmail.com>
2021-12-18 22:48:21 +0000

    created login page and began incorporating wtforms

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import (Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, Stringfield
 if os.path.exists("env.py"):
     import env
 from bson.objectid import ObjectId
@@ -13,12 +15,20 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+class LoginForm(FlaskForm):
+    username = Stringfield('username')
+    password = PasswordField('password')
 
 @app.route("/")
 @app.route("/get_strong")
 def get_strong():
     workouts = mongo.db.workouts.find()
     return render_template("get_strong.html", workouts=workouts)
+
+@app.route("/login", methods=['GET','POST']) 
+def login():
+    login_form = LoginForm()
+    return render_template("login.html", login_form=login_form)
 
 
 if __name__ == "__main__":
