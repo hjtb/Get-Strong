337be1eaadbd594bb848225ca4e4326e394bd0c5
hjtb <willholtonjtb@gmail.com>
2021-12-30 17:07:02 +0000

    getting the db connected

--- website/models.py
+++ website/models.py
@@ -11,13 +11,22 @@ class User(db.Document, UserMixin):
     This provides default implementations for the methods that Flask-Login
     expects user objects to have.
     """
-    def __init__(self, user_mongo):
+
+    email = db.StringField(max_length=60)
+    username = db.StringField(max_length=60)
+
+    def get_id(self):
+        object_id = self.id
+        return str(object_id)
+
+        
+"""    def __init__(self, user_mongo):
         self.id = user_mongo.get("_id")
         self.email = user_mongo.get('email')
         self.username = user_mongo.get('username')
         self.workouts = user_mongo.get('workouts')
         self.is_admin = user_mongo.get('is_admin')
+"""
 
-    def get_id(self):
-        object_id = self.id
-        return str(object_id)
+
+
--- website/routes.py
+++ website/routes.py
@@ -7,8 +7,13 @@ from flask_login import (
 )
 from flask import current_app as app
 from website import db
+from website.models import User
 
 
 @app.route("/index")
 def index():
+    user = User(email="a", username="b")
+    users = User.objects.all()
+    for user in users:
+        print(user)
     return "hello"
