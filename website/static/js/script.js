d034a8ca33c5e58e417c605015780887585dd335
Will Holton <willholtonjtb@gmail.com>
2022-01-31 21:59:43 +0000

    added comments to javascript file

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -1,38 +1,39 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('.flash').fadeIn('slow').delay(2000).fadeOut(5000);
-    $('#exercise-row select').formSelect();
-    $('#comments').val('');
-    $("#delete-exercise-button").hide();
-    $("#template-1").hide();
-    M.textareaAutoResize($('#comments'));
-    var counter = 0;
-    $("#next-exercise-button").click(function(){
-      counter++;
-      if (counter >= 10){
-        $(this).hide();
-      }
-      console.log(counter);
-      var rowId = 'added-exercise-row' + '_' + counter;
-      $("#template-1").clone().attr('id', rowId).addClass('added-exercise').appendTo("#exercises-list").show();
-      $("#delete-exercise-button").show();
-      console.log('options');
-      var $newRow = $(`#${rowId}`);
-      console.log($newRow.contents());
-      $(`#${rowId} select`).formSelect();
-    });
-    $("#delete-exercise-button").click(function(){
-      counter--;
-      if (counter < 10){
-        $("#next-exercise-button").show();
-      }
-      console.log(counter);
-      if (counter == 0){
-        $("#delete-exercise-button").hide();
-      };
-      $(".added-exercise:last-of-type").remove();
-    });
-    if (counter > 0){
-      $("#delete-exercise-button").show();
+  $('.sidenav').sidenav();
+  $('.flash').fadeIn('slow').delay(2000).fadeOut(5000);
+  $('#exercise-row select').formSelect();
+  $('#comments').val('');
+  $("#delete-exercise-button").hide();
+  $("#template-1").hide();
+  M.textareaAutoResize($('#comments'));
+  // generate our exercise rows dynamically using buttons
+  var counter = 0;
+  $("#next-exercise-button").click(function(){
+    counter++;
+    // set the limit of rows that can be generated
+    if (counter >= 10){
+      $(this).hide();
+    }
+    // Generate new ids for each row 
+    var rowId = 'added-exercise-row' + '_' + counter;
+    // Clone our new rows from the hidden template row
+    $("#template-1").clone().attr('id', rowId).addClass('added-exercise').appendTo("#exercises-list").show();
+    // Only show our delete row button for newly created rows
+    $("#delete-exercise-button").show();
+    // initialise the select dropdown for materialize on each of the new rows
+    $(`#${rowId} select`).formSelect();
+  });
+  // delete row functionality on the delete button
+  $("#delete-exercise-button").click(function(){
+    counter--;
+    // If we delete the final row we allow the row to be regenerated
+    if (counter < 10){
+      $("#next-exercise-button").show();
+    }
+    // If we go back to the initial row being the only left we remove the delete option
+    if (counter == 0){
+      $("#delete-exercise-button").hide();
     };
+    $(".added-exercise:last-of-type").remove();
+  });
 });
