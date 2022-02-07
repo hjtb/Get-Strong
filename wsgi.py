15e5520baef64e2d506387f01958da183174b5f3
Will Holton <willholtonjtb@gmail.com>
2022-02-07 18:56:57 +0000

    fixing wsgi for heroku

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,10 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    #app.run(host='0.0.0.0', port='5000', debug=True)
     #app.run(host=os.environ.get("IP"),
      #   port=int(os.environ.get("PORT")),
       #  debug=True)pi
+    app.run(host=os.environ.get("IP"),
+    port=int(os.environ.get("PORT")),
+    debug=True)
